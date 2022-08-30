6ab100c2145c9dc5023bd69d4e29d69bf07cf9cb
tyler <tyler.hagstrom@gmail.com>
2022-08-29 19:32:14 -0500

    The beginning of being able to submit the form.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RequestForm.Controllers;
 using RequestForm.Interfaces;
@@ -9,7 +10,7 @@ namespace RequestForm.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private DBInterface db = new sqliteController();
+    private readonly DBInterface db = new sqliteController();
     public NewEmployeeForm employeeInfo = new NewEmployeeForm();
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -23,5 +24,37 @@ public class IndexModel : PageModel
         employeeInfo.Softwares = db.GetAllSoftware();
     }
 
+    public IActionResult OnPost()
+    {
+        string emailBody = BuildEmail(Request.Form);
+        return RedirectToPage("./Index");
+    }
+
+    private string BuildEmail(IFormCollection form)
+    {
+        if (form == null) return String.Empty;
+
+        StringBuilder drives = new StringBuilder();
+        StringBuilder software = new StringBuilder();
+
+        if (form["drive_checked"].Count > 0)
+        {
+            foreach(string drive in form["drive_checked"])
+            {
+                drives.Append(drive);
+            }
+        }
+
+        if (form["drive_checked"].Count > 0)
+        {
+            foreach (string drive in form["software_checked"])
+            {
+                software.Append(drive);
+            }
+        }
+
+        return "Drives: "+drives.ToString() + " Software: " + software.ToString();
+    }
 }
+
 
