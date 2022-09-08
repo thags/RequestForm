3974a2c8dcc6a968007881cf61a8f5f87180aa2d
tyler <tyler.hagstrom@gmail.com>
2022-09-08 15:56:56 -0500

    Setting up for edit drives page

---
+++ Pages/EditDrives.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RequestForm.Controllers;
+using RequestForm.Interfaces;
+using RequestForm.Models;
+
+namespace RequestForm.Pages;
+
+public class EditDrivesModel : PageModel
+{
+    private readonly DBInterface db = new sqliteController();
+    public List<Drive> drives = new List<Drive>();
+
+
+    public void OnGet()
+    {
+        drives = db.GetAllDrives();
+    }
+
+    
+}
+
+
