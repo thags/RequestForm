ac89e7a110f678f25f37403064e39c75aab4e9bd
Tyler Hagstrom <tyler.hagstrom@zelis.com>
2022-09-08 20:12:06 -0500

    Added requirements to form

--- Models/NewEmployeeForm.cs
+++ Models/NewEmployeeForm.cs
@@ -1,12 +1,20 @@
-﻿namespace RequestForm.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RequestForm.Models
 {
     public class NewEmployeeForm
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string JobTitle { get; set; }
+        [Required]
         public List<Drive> Drives { get; set; }
+        [Required]
         public string ManagerName { get; set; }
+        [Required]
         public List<Software> Softwares { get; set; }
     }
 }
