e53b84cfa6d55e259f98dae3ce33e50649763607
Tyler Hagstrom <tyler.hagstrom@zelis.com>
2022-09-09 16:39:30 -0500

    EditDrives page now allows the user to create new drive, delete a drive or edit a drive

--- Pages/EditDrives.cshtml.cs
+++ Pages/EditDrives.cshtml.cs
@@ -15,9 +15,50 @@ public class EditDrivesModel : PageModel
 
     public void OnGet()
     {
-        drives = db.GetAllDrives();
+        GetDrives();
     }
-    
+
+    public void GetDrives()
+    {
+        drives = db.GetAllDrives();
+        drives.Add(
+            new Drive()
+            {
+                Id = -1,
+            });
+    }
+
+    public IActionResult OnPost(IFormCollection form)
+    {
+        GetDrives();
+        int index = int.Parse(form.ToList()[0].Value);
+        Drive? driveEditing = drives.Find(x => x.Id == index);
+
+        if (driveEditing == null)
+        {
+            return RedirectToPage("./EditDrives");
+        }
+
+        driveEditing.DriveLetter = form.ToList()[1].Value;
+        driveEditing.DriveName = form.ToList()[2].Value;
+
+        if (String.IsNullOrEmpty(driveEditing.DriveName) && String.IsNullOrEmpty(driveEditing.DriveLetter))
+        {
+            db.DeleteDrive(driveEditing.Id);
+            return RedirectToPage("./EditDrives");
+        }
+        
+        if (driveEditing.Id == -1)
+        {
+            db.AddDrive(driveEditing);
+            return RedirectToPage("./EditDrives");
+        }
+
+        db.EditDrive(driveEditing);
+
+        return RedirectToPage("./EditDrives");
+    }
+
 }
 
 
